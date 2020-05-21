4f91c952fc4623d4d58e5ecc515e704ab615b8c8
kaoru <kaoru@birman-mac.local>
2020-05-22 02:01:49 +0900

    add react-router and change webpack tsconfig setting for react-router

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,47 @@
-const path = require('path');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
+const path = require("path");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
 module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
+  mode: "development",
+  entry: "./src/index.tsx",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/js'),
-    publicPath: '/js/',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public/js"),
+    publicPath: "/js/"
   },
   resolve: {
     alias: {
-      components: path.resolve(__dirname, 'src/conponents'),
-      containers: path.resolve(__dirname, 'src/containers'),
-      scss: path.resolve(__dirname, 'src/scss'),
-      src: path.resolve(__dirname, 'src'),
+      components: path.resolve(__dirname, "src/components"),
+      containers: path.resolve(__dirname, "src/containers"),
+      scss: path.resolve(__dirname, "src/scss"),
+      styles: path.resolve(__dirname, "styles"),
+      src: path.resolve(__dirname, "src")
     },
-    extensions: ['.ts', '.tsx', '.js', '.json'],
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: ["babel-loader", "eslint-loader"]
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-    ],
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+      }
+    ]
   },
   plugins: [
     new StyleLintPlugin({
-      configFile: '.stylelintrc.json',
-    }),
+      configFile: ".stylelintrc.json"
+    })
   ],
   devServer: {
     open: true,
     port: 9000,
-    contentBase: './public',
+    contentBase: "./public",
+    historyApiFallback:true,
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map"
 };
